e19f1e4e4700e23ff722dd296d39eeeb1f453a6f
Timo Stramann <21283057+TimoStramann@users.noreply.github.com>
2019-09-27 16:44:55 +0200

    preparing system packages for nginx support
    install different packages for nginx and apache so not both webservers are installed at the sam time

--- basic/system.bash
+++ basic/system.bash
@@ -23,29 +23,70 @@ setupSystem() {
   # for now they are the same but Ubuntu is likely to be updated more recently
   INSTALL_PKGsDEBIAN="apt-utils debconf-utils clamav clamav-daemon \
                   dialog apache2-utils mcrypt curl bzip2 zip unzip tar wget git \
-                  apache2 php php-fpm php-json php-gd php-imagick imagemagick php-curl php-mcrypt \
-                  php-xsl php-fileinfo php-uploadprogress php-geoip geoip-database-contrib \
+                  php php-fpm php-json php-gd php-imagick imagemagick php-curl php-mcrypt \
+                  php-xsl php-fileinfo php-geoip geoip-database-contrib \
                   php-apcu php-bcmath php-dom php-gnupg php-imap php-mailparse php-mbstring \
                   php-memcached php-mysql php-pdo php-pdo-mysql php-sqlite3 sqlite3 \
                   php-pspell spell aspell-de php-phar php-posix php-pear php-tidy tidy \
                   php-yaml php-zip php-intl php-memcache php-xmlrpc rkhunter certbot \
-                  nscd libnss-extrausers apache2-suexec-pristine bind9 logrotate awstats"
+                  nscd libnss-extrausers bind9 logrotate awstats vim composer"
   INSTALL_PKGsUBUNTU="apt-utils debconf-utils clamav clamav-daemon \
                   dialog apache2-utils mcrypt curl bzip2 zip unzip tar wget git \
-                  apache2 php php-fpm php-json php-gd php-imagick imagemagick php-curl php-mcrypt \
-                  php-xsl php-fileinfo php-uploadprogress php-geoip geoip-database-contrib \
+                  php php-fpm php-json php-gd php-imagick imagemagick php-curl php-mcrypt \
+                  php-xsl php-fileinfo php-geoip geoip-database-contrib \
                   php-apcu php-bcmath php-dom php-gnupg php-imap php-mailparse php-mbstring \
                   php-memcached php-mysql php-pdo php-pdo-mysql php-sqlite3 sqlite3 \
                   php-pspell spell aspell-de php-phar php-posix php-pear php-tidy tidy \
                   php-yaml php-zip php-intl php-memcache php-xmlrpc rkhunter certbot \
-                  nscd libnss-extrausers apache2-suexec-pristine bind9 logrotate awstats"
+                  nscd libnss-extrausers bind9 logrotate awstats vim composer"
+  INSTALL_PKGsDEBIAN_apache="apache2 apache2-suexec-pristine php-uploadprogress"
+  INSTALL_PKGsUBUNTU_apache=" apache2 apache2-suexec-pristine php-uploadprogress"
+  INSTALL_PKGsDEBIAN_nginx="nginx"
+  INSTALL_PKGsUBUNTU_nginx="nginx"
+
+  if [ "$webserverChoice" = "" ] || [ -z $webserverChoice ]; then
+    #nohup is default now
+	if command -v apache2 2>/dev/null; then
+	  webserverChosen="apache"
+	  return 0
+	fi
+  elif [ "$installMethodeChoice" = "apache" ]; then
+  	if command -v apache2 2>/dev/null; then
+	  webserverChosen="apache"
+	  return 0
+	fi
+  elif [ "$installMethodeChoice" = "nginx" ]; then
+  	if command -v nginx 2>/dev/null; then
+	  webserverChosen="nginx"
+	  return 0
+	fi
+  else
+    echo -e "\e[31mError choosing webserver.\e[0m"
+    exit 1
+  fi
 
   if [ $DISTRO = "Debian" ]; then
     cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $INSTALL_PKGsDEBIAN";
     _evalBg "${cmd}";
+	if [ $webserverChosen = "apache" ]; then
+	  cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $INSTALL_PKGsDEBIAN_apache";
+	  _evalBg "${cmd}";
+	fi
+	if [ $webserverChosen = "nginx" ]; then
+	  cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $INSTALL_PKGsDEBIAN_nginx";
+	  _evalBg "${cmd}";
+	fi
   elif [ $DISTRO = "Ubuntu" ]; then
     cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $INSTALL_PKGsUBUNTU";
     _evalBg "${cmd}";
+	if [ $webserverChosen = "apache" ]; then
+	  cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $INSTALL_PKGsUBUNTU_apache";
+	  _evalBg "${cmd}";
+	fi
+	if [ $webserverChosen = "nginx" ]; then
+	  cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y $INSTALL_PKGsUBUNTU_nginx";
+	  _evalBg "${cmd}";
+	fi
   else
     echo "No methode for other OS than Debian/Ubuntu prepared yet"
   fi
