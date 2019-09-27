69a1d179b5c8f5e8e6cad639def242f93e118a76
Timo Stramann <21283057+TimoStramann@users.noreply.github.com>
2019-09-27 18:18:52 +0200

    phpmyadmin is optional
    since most distros do no longer contain phpmyadmin prebuild packages we make prepare it for older releases only

--- basic/phpmyadmin.bash
+++ basic/phpmyadmin.bash
@@ -4,13 +4,15 @@
 #---------------------------------------------------------------------
 installPHPmyAdmin() {
 
-  echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
-  echo "phpmyadmin phpmyadmin/dbconfig-install boolean false" | debconf-set-selections
-  echo "phpmyadmin phpmyadmin/dbconfig-upgrade boolean false" | debconf-set-selections
-  # echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_PASS" | debconf-set-selections
-  # echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | debconf-set-selections
-  start_spinner "Installing phpmyadmin..."
-  cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y phpmyadmin";
-  _evalBg "${cmd}";
+  if ([ $DISTRO = "Debian" ] && [ $DISTROv -eq "9" ]) || ([ $DISTRO = "Ubuntu" ] && [ $DISTROv -eq "16" ]); then
+    echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+    echo "phpmyadmin phpmyadmin/dbconfig-install boolean false" | debconf-set-selections
+    echo "phpmyadmin phpmyadmin/dbconfig-upgrade boolean false" | debconf-set-selections
+    # echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_PASS" | debconf-set-selections
+    # echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | debconf-set-selections
+    start_spinner "Installing phpmyadmin..."
+    cmd="sudo DEBIAN_FRONTEND=noninteractive apt-get install -y phpmyadmin";
+    _evalBg "${cmd}";
+  fi
 
 }
