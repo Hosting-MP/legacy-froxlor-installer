70aac1915b73b7c9c23b182556662ade6092b7ad
Timo Stramann <21283057+TimoStramann@users.noreply.github.com>
2019-09-27 16:45:51 +0200

    php-fpm for either nginx or apache
    php-fpm will be activated on either nginx or apache2

--- basic/php-fpm.bash
+++ basic/php-fpm.bash
@@ -16,22 +16,29 @@ setupPHPfpm() {
   start_spinner "Making PHP fpm start on boot"
   cmd="systemctl enable php$PHPv-fpm";
   _evalBg "${cmd}";
+  
+  if [ $webserverChosen = "apache" ]; then
+    start_spinner "Enable apache2 modules"
+    cmd="a2enmod rewrite ssl http2 proxy_fcgi setenvif actions headers suexec";
+    _evalBg "${cmd}";
 
-  start_spinner "Enable apache2 modules"
-  cmd="a2enmod rewrite ssl proxy_fcgi setenvif actions headers suexec";
-  _evalBg "${cmd}";
+    start_spinner "Enable PHP fpm for apache2"
+    cmd="a2enconf php$PHPv-fpm";
+    _evalBg "${cmd}";
 
-  start_spinner "Enable PHP fpm for apache2"
-  cmd="a2enconf php$PHPv-fpm";
-  _evalBg "${cmd}";
+    restartApache
 
-  restartApache
+    # disable mod_php to use fpm from now on
+    start_spinner "Disable mod php for apache2"
+    cmd="a2dismod php$PHPv";
+    _evalBg "${cmd}";
 
-  # disable mod_php to use fpm from now on
-  start_spinner "Disable mod php for apache2"
-  cmd="a2dismod php$PHPv";
-  _evalBg "${cmd}";
+    restartApache
+  fi
+  if [ $webserverChosen = "nginx" ]; then
+	sed -i 's!#location ~ \\.php$ {!location ~ \\.php$ { fastcgi_pass unix:/run/php/php'"$PHPv"'-fpm.sock; }!g' /etc/nginx/sites-enabled/default
 
-  restartApache
+    restartNginx
+  fi
 
 }
