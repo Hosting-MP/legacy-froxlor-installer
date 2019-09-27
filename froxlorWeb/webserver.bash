c0f0cee3e19df43cdc99258b6ad477234607ee63
Timo Stramann <21283057+TimoStramann@users.noreply.github.com>
2019-09-27 21:52:24 +0200

    add nginx support for webserver config
    adding letsencrypt config for nginx

--- froxlorWeb/webserver.bash
+++ froxlorWeb/webserver.bash
@@ -9,12 +9,31 @@ setupWebServer() {
   mkdir -p /var/customers/logs/
   mkdir -p /var/customers/tmp
   chmod 1777 /var/customers/tmp
-  # add Lets Encrypt cases
-cat <<EOF > /etc/apache2/conf-enabled/acme.conf
+
+
+  if [ "$webserverChosen" = "apache" ]; then
+    # add Lets Encrypt cases
+    cat <<EOF > /etc/apache2/conf-enabled/acme.conf
 Alias "/.well-known/acme-challenge" "/var/www/html/.well-known/acme-challenge"
 <Directory "/var/www/html/.well-known/acme-challenge">
     Require all granted
 </Directory>
 EOF
+
+    restartApache
+  fi
+  if [ "$webserverChosen" = "nginx" ]; then
+  cat <<EOF > /etc/nginx/acme.conf
+location /.well-known/acme-challenge {
+    alias /var/www/html/.well-known/acme-challenge;
+
+    location ~ /.well-known/acme-challenge/(.*) {
+        default_type text/plain;
+    }
+}
+EOF
+
+    restartNginx
+  fi
 
 }
