bbed37b52eb84052ef7befe398d33b4415daf61c
Timo Stramann <21283057+TimoStramann@users.noreply.github.com>
2020-02-08 23:11:06 +0100

    Removing unnecessary slashes

--- froxlorWeb/awstats.bash
+++ froxlorWeb/awstats.bash
@@ -7,8 +7,8 @@ setupAwstats() {
   # setting up awstats according to froxlor config
   cp /usr/share/awstats/tools/awstats_buildstaticpages.pl /usr/bin/
   ln -s /etc/awstats/awstats.conf /etc/awstats/awstats.model.conf
-  sed -i.bak 's/^DirData/# DirData/' /etc/awstats//awstats.model.conf
-  sed -i.bak 's|^\\(DirIcons=\\).*$|\\1\\"/awstats-icon\\"|' /etc/awstats//awstats.model.conf
+  sed -i.bak 's/^DirData/# DirData/' /etc/awstats/awstats.model.conf
+  sed -i.bak 's|^\\(DirIcons=\\).*$|\\1\\"/awstats-icon\\"|' /etc/awstats/awstats.model.conf
   rm /etc/cron.d/awstats
 
 }
